1ee7c005362049aa8caae57f989f3d2b4f8e5d5a
Petyr-CZ <novakpetr531@gmail.com>
2018-10-24 13:36:00 +0200

    Update druhak.php

--- app/web/tridy/druhak.php
+++ app/web/tridy/druhak.php
@@ -1,10 +1,258 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="dyzajn.css" rel="stylesheet" type="text/css">
+
+
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Třídy</title>
+    
+
   </head>
   <body>
-    <h1>EMPTY</h1>
-  </body>
+	<div class="dropseznam">
+	  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Výběr stránky
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="http://www.jakub-pelc.8u.cz/projects/ca2018/projectX/index.php">MENU</a>
+	  <a class="dropdown-item" href="./prima.php">Prima</a>
+      <a class="dropdown-item" href="./sekunda.php">Sekunda</a>
+      <a class="dropdown-item" href="./tercie.php">Tercie</a>
+	  <a class="dropdown-item" href="./kvarta.php">Kvarta</a>
+	  <a class="dropdown-item" href="./kvinta.php">Kvinta</a>
+	  <a class="dropdown-item" href="./sexta.php">Sexta</a>
+	  <a class="dropdown-item" href="./septima.php">Septima</a>
+	  <a class="dropdown-item" href="./oktava.php">Oktáva</a>
+	  <a class="dropdown-item" href="./prvak.php">První ročník</a>
+	  <a class="dropdown-item" href="./druhak.php">Druhý ročník</a>
+	  <a class="dropdown-item" href="./tretak.php">Třetí Ročník</a>
+	  <a class="dropdown-item" href="./ctvrtak.php">Čtvrtý ročník</a>
+  </div>
+</div>
+	  </div>	  
+
+	  <header><center><strong> 4. leté gymnázium - Druhý ročník </header></center></strong>
+	  
+  <table style="width: 100%; height: 30%;">
+
+  <tr class="tabel">
+        <td width="20%"> </td>
+        <td width="20%"> Předměty </td>
+        <td width="5%"> </td>
+        <td width="20%"> Události </td>
+        <td width="5%"> </td>
+        <td width="20%"> Sporty </td>
+        <td width="10%"> </td>
+  </tr>
+
+<tr align="left" class="tabel2">
+      <td> </td>
+      <td> Český jazyk </td>
+      <td> </td>
+      <td> Adaptační den </td>
+      <td> </td>
+      <td> Basketbal </td>
+      <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Anglický jazyk </td>
+    <td> </td>
+    <td><a href="https://sites.google.com/a/gbn.cz/tv/sportovni-kurzy/lyzarsky-kurz-kvinta"> Lyžařský kurz </a></td>
+    <td> </td>
+    <td> Volejbal </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Druhý cizí jazyk </td>
+    <td> </td>
+    <td> Výměnný pobyt - Dánsko </td>
+    <td> </td>
+    <td> Sbor </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Matematika </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> Twirling </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Fyzika </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> Atletika </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Základy společenských věd </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> Fotbal </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Zeměpis </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> Florbal </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Biologie </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> Gymnastika </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Dějepis </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Informatika </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Chemie </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+
+  <tr align="left" class="tabel2">
+    <td> </td>
+    <td> Hudební / výtvarná výchova </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+  </tr>
+</table>
+
+
+</body>
+
+<button type="button"  data-toggle="modal" data-target="#exampleModal"> Launch demo admin </a> </button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Formulář</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="space">
+        </div>
+        <div class="container">
+          <form>
+            <label for="formular">Název</label> <label for="linka">Link</label><br>
+            <i><input type="text" placeholder="Název" class="vypln"></i><br>
+            <label for="formular">Typ</label><br>
+            <i><input type="text" placeholder="Událost, kurz..." class="vypln"></i><br>
+
+            <label for="formular">Datum</label><br>
+            <table>
+              <th>
+                <tr>
+                  od <select>
+                    <option value="1">Leden</option>
+                    <option value="2">Únor</option>
+                    <option value="3">Březen</option>
+                    <option value="4">Duben</option>
+                    <option value="5">Květen</option>
+                    <option value="6">Červen</option>
+                    <option value="7">Červenec</option>
+                    <option value="8">Srpen</option>
+                    <option value="9">Září</option>
+                    <option value="10">Říjen</option>
+                    <option value="11">Listopad</option>
+                    <option value="12">Prosinec</option>
+                  </select>
+                </tr>
+                <tr>
+                  do <select>
+                    <option value="1">Leden</option>
+                    <option value="2">Únor</option>
+                    <option value="3">Březen</option>
+                    <option value="4">Duben</option>
+                    <option value="5">Květen</option>
+                    <option value="6">Červen</option>
+                    <option value="7">Červenec</option>
+                    <option value="8">Srpen</option>
+                    <option value="9">Září</option>
+                    <option value="10">Říjen</option>
+                    <option value="11">Listopad</option>
+                    <option value="12">Prosinec</option>
+                  </select><br>
+                </tr>
+              </th>
+            </table>
+
+
+            <label for="formular">Podrobnosti</label><br>
+            <textarea rows="4" placeholder="..." class="box"></textarea><br>
+            <div class="linka">
+               <i><input class="linka" type="text" placeholder="odkaz" class="vypln"></i>
+            </div>
+            <div class="linka">
+               <i><input class="linka" type="text" placeholder="koment" class="vypln"></i>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </html>
