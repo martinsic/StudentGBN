5f3b7527c4029064dfdda66b99f61ec5f1410cd9
Petyr-CZ <novakpetr531@gmail.com>
2018-10-24 16:58:01 +0200

    Add files via upload

---
+++ app/web/vs_a_maturita.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="passwd.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <!-- Button trigger modal -->
+
+<a href="http://www.gbn.cz/pro-zaky/maturitni-zkousky"><button type="button" class="btn btn-warning" data-toggle="modal" data-target="#maturita_button">
+Maturita
+</button></a>
+
+<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#vs_button">
+  Vysoké školy
+</button>
+
+
+<!-- Modal -->
+<div class="modal fade" id="vs_button" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Vysoké školy</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <center><strong><u>Kontakt na výchovného poradce</strong></u><br>
+        Eliška Pilátová  <br>
+        pilatova@gbn.cz<br>
+        tel.: 311 444 644 </center> <br>
+         <br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zavřít</button>
+        <a class="btn btn-primary" href="https://www.vysokeskoly.cz" role="button">Přejít na stránku vysoké školy.cz</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
