36436ff5c496a41433a59af8331321c9cea56cfb
jajsemmatej <jajsemmatej@gmail.com>
2018-10-24 08:31:00 +0200

    Update verify.php

--- app/tridy/verify.php
+++ app/tridy/verify.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['login']) && isset($_POST['password'])){
 	include_once "users.php";
-      if($users[1] == $_POST['login'] && $pwds[1] == $_POST['password']){
+      if($users[1] == $_POST['login'] && $pwds[1] == hash("sha512", $_POST['password'])){
         include_once "hash.php";
         $VarToCookie = $_SERVER['REMOTE_ADDR'] . date("Y-m-d") . "<dmx>" . $ids[1];
         $CryptVarToCookie = my_hash_crypt($VarToCookie,'e');
