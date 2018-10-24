c786045bd56eb699a24edff2bc2759ca60b106dc
Petyr-CZ <novakpetr531@gmail.com>
2018-10-24 13:44:31 +0200

    Update sekunda.php

--- app/web/tridy/sekunda.php
+++ app/web/tridy/sekunda.php
@@ -62,7 +62,7 @@
     <td> </td>
     <td> Anglický jazyk </td>
     <td> </td>
-    <td><a href="https://sites.google.com/a/gbn.cz/tv/sportovni-kurzy/lyzarsky-kurz-kvinta"> Lyžařský kurz </a></td>
+    <td><a href="https://sites.google.com/a/gbn.cz/tv/sportovni-kurzy/lyzarsky-kurz-sekunda"> Lyžařský kurz </a></td>
     <td> </td>
     <td> Volejbal </td>
     <td> </td>
