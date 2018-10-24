3ed8ac526ef4a0f708a0efcee13b93ea7dc22884
jajsemmatej <jajsemmatej@gmail.com>
2018-10-24 08:32:05 +0200

    Update users.php

--- app/tridy/users.php
+++ app/tridy/users.php
@@ -1,5 +1,5 @@
 <?php
 $users[1] = "gymnazium@gbn.cz";
-$pwds[1] = "gymnazium1703gbn";
+$pwds[1] = "d43e069b983df2158a3a273dd1bf3d2f5dc889ff3004e2624ec00327bee9978aa1e4c58fb900cf2f1c832cdb7b324a32aa8b32c3a45237f9791418b47ffe82d2";
 $ids[1] = 1;
 ?>
